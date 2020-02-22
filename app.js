fc0b9d7b8df9ef80595739610896bef110a57513
Connor Knabe <ConnorKnabe@gmail.com>
2020-02-21 18:48:29 -0600

    adds songs to playlist correctly

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const log4js = require('log4js');
 var logger = log4js.getLogger();
 logger.level = 'debug';
-const login = require('./login.js');
+const music = require('./music.js');
 const options = require('./options.js');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
@@ -14,20 +14,15 @@ rl.setDefaultOptions({
 	history: false
 });
 
-// var pw = rl.question(`Please enter your Apple ID pw:`, {
-// 	hideEchoBack: true // The typed text on screen is hidden by `*` (default).
-// });
-
-// main(pw);
 main();
 
 async function main(pw) {
+	var artistNumber = 0;
 	const browser = await puppeteer.launch({ headless: options.disableBrowserWindow, devtools: false, timeout: 60000, slowMo: 250 });
 	try {
 		// @ts-ignore
 		const page = await browser.newPage();
 		await page.goto('https://musi.sh', { waitUntil: 'networkidle2' });
-
 		await page.click('._045a');
 
 		// debugger;
@@ -42,28 +37,41 @@ async function main(pw) {
 			await addSong(page, 3);
 			await addSong(page, 4);
 			await addSong(page, 5);
+
+			console.log(artistNumber);
+			await addNextArtist(page, artistNumber++);
 		});
 
-		array.forEach((element) => {});
-
 		await page.waitFor('._69de');
-		await page.goto('https://musi.sh/search/catalog/zedd', { waitUntil: 'networkidle2' });
+		browser.headless = true;
+
+		await page.goto(`https://musi.sh/search/catalog/${music.list[artistNumber]}`, { waitUntil: 'networkidle2' });
 	} catch (exception) {
 		await browser.close();
 	}
 
 	async function addSong(page, songNumber) {
-		const songTitle = await page.$(`#main-content > div > div._657c > div > div:nth-child(${songNumber})`);
+		try {
+			const songTitle = await page.$(`#main-content > div > div._657c > div > div:nth-child(${songNumber})`);
 
-		await songTitle.click({
-			button: 'right'
-		});
-		await page.click('#app-root > nav.react-contextmenu.react-contextmenu--visible > div:nth-child(8)');
+			await songTitle.click({
+				button: 'right'
+			});
+			await page.click('#app-root > nav.react-contextmenu.react-contextmenu--visible > div:nth-child(8)');
 
-		await page.waitForSelector('._92f0');
-		await page.click('#main-content > div._1a01 > div > div > div:nth-child(1)');
-		await page.waitFor(1000);
+			await page.waitForSelector('._92f0');
+			await page.click('#main-content > div._1a01 > div > div > div:nth-child(1)');
+			await page.waitFor(1000);
+			return await Promise.resolve();
+		} catch (err) {
+			console.error(`Issue finding song for ${music.list[artistNumber]}`);
+			console.error('err', err);
+			return await Promise.resolve();
+		}
+	}
 
-		return await Promise.resolve();
+	async function addNextArtist(page, i) {
+		await page.waitFor('._69de');
+		await page.goto(`https://musi.sh/search/catalog/${music.list[i]}`, { waitUntil: 'networkidle2' });
 	}
 }
